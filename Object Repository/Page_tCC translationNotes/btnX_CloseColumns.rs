3c24d068caaa49fb21130cb34ea873882caf1334
Chris Kosieracki <cckozie@hotmail.com>
2020-10-05 18:52:11 -0500

    Changed btnX_CloseColumns to use attributes instead of xpath

--- Object Repository/Page_tCC translationNotes/btnX_CloseColumns.rs
+++ Object Repository/Page_tCC translationNotes/btnX_CloseColumns.rs
@@ -13,8 +13,12 @@
          <key>XPATH</key>
          <value>(//button[@type='button'])[179]</value>
       </entry>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@type = 'button' and @aria-label = 'Close']</value>
+      </entry>
    </selectorCollection>
-   <selectorMethod>XPATH</selectorMethod>
+   <selectorMethod>BASIC</selectorMethod>
    <useRalativeImagePath>true</useRalativeImagePath>
    <webElementProperties>
       <isSelected>false</isSelected>
