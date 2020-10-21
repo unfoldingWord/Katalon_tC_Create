60d7b7c43a5d2e140ab836b65c5b9dad9bac2ab2
Chris Kosieracki <cckozie@hotmail.com>
2020-10-21 14:59:08 -0500

    Update Test Permissions for invalid password

--- Scripts/tCC Test Cases/tCC Test Permissions/Script1598999042176.groovy
+++ Scripts/tCC Test Cases/tCC Test Permissions/Script1598999042176.groovy
@@ -16,34 +16,34 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import com.kms.katalon.core.util.KeywordUtil as KeywordUtil
 
-WebUI.callTestCase(findTestCase('tCC Components/tCC Login'), [('user') : 'tc01', ('password') : 'tc01'], FailureHandling.STOP_ON_FAILURE)
+// USER WITH WRITE AUTHORITY IN ORGANIZATION
+WebUI.callTestCase(findTestCase('tCC Components/tCC Login'), [('user') : 'tc01', ('password') : 'tc01', ('newSession') : true], FailureHandling.STOP_ON_FAILURE)
 
 if (!(WebUI.callTestCase(findTestCase('tCC Components/tCC Select Org-Lang-Resource'), [('organization') : '', ('language') : ''
         , ('resource') : 'unfoldingWord® Translation Academy'], FailureHandling.STOP_ON_FAILURE))) {
     KeywordUtil.markFailed('Exiting script because organization was not found.')
 	CustomKeywords.'unfoldingWord_Keywords.SendMessage.SendFailMessage'('Test failed because the organization was not found.')
-	
-    WebUI.closeBrowser()
-
-    return null
 }
 
 WebUI.closeBrowser()
 
-WebUI.callTestCase(findTestCase('tCC Components/tCC Login'), [('user') : 'tc02', ('password') : 'tc02'], FailureHandling.STOP_ON_FAILURE)
+// USER WITH NO ORGANIZATION
+WebUI.callTestCase(findTestCase('tCC Components/tCC Login'), [('user') : 'tc02', ('password') : 'tc02', ('newSession') : true], FailureHandling.STOP_ON_FAILURE)
 
-if (!(WebUI.callTestCase(findTestCase('tCC Components/tCC Select Org-Lang-Resource'), [('organization') : '', ('language') : ''
-        , ('resource') : 'unfoldingWord® Translation Academy'], FailureHandling.OPTIONAL))) {
-    if (!WebUI.verifyElementPresent(findTestObject('Page_tC Create/msg_No organizations for this account'), 1, FailureHandling.OPTIONAL)) {
-		msg = 'Exiting script because organization was not found and no error message was displayed.'
-		CustomKeywords.'unfoldingWord_Keywords.SendMessage.SendFailMessage'(msg)
-    } else {
-		println('No organizations for this account message was displayed.')
-    }
-    WebUI.closeBrowser()
-
-    return null
+if (!WebUI.verifyElementPresent(findTestObject('Page_tC Create/msg_No organizations for this account'), 1, FailureHandling.OPTIONAL)) {
+	println = 'ERROR: The no organization error message was not displayed.'
+	CustomKeywords.'unfoldingWord_Keywords.SendMessage.SendFailMessage'('Test failed because the no organization error message was not displayed.')
 }
 
+WebUI.closeBrowser()
+
+// USER WITH INVALID PASSWORD
+WebUI.callTestCase(findTestCase('tCC Components/tCC Login'), [('user') : 'tc02', ('password') : 'tc111', ('newSession') : true], FailureHandling.STOP_ON_FAILURE)
+
+if (!WebUI.verifyElementPresent(findTestObject('Page_tC Create/msg_Password is Invalid'), 1, FailureHandling.OPTIONAL)) {
+	println('Invalid password message was not displayed')
+	CustomKeywords.'unfoldingWord_Keywords.SendMessage.SendFailMessage'('Test failed because the invalid password message was not displayed.')
+} 
+		
 WebUI.closeBrowser()
 
