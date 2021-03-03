951a939397aa20a0c16244c8bd1b3e838fb1cc01
Chris Kosieracki <cckozie@hotmail.com>
2021-03-03 13:45:21 -0600

    Tweaked a couple of tN objects

--- Object Repository/Page_tCC translationNotes/label_OrigQuote-previous2.rs
+++ Object Repository/Page_tCC translationNotes/label_OrigQuote-previous2.rs
@@ -1,21 +1,17 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <WebElementEntity>
    <description></description>
-   <name>label_OrigQuote</name>
+   <name>label_OrigQuote-previous2</name>
    <tag></tag>
-   <elementGuidId>d786020c-5ed6-4e99-a168-0e42b7a1e7ee</elementGuidId>
+   <elementGuidId>297e137a-afc0-4ca4-830d-eaca7a89c133</elementGuidId>
    <selectorCollection>
       <entry>
-         <key>BASIC</key>
-         <value>id(&quot;MUIDataTableBodyRow-3&quot;)/td[@class=&quot;MuiTableCell-root-222 MuiTableCell-body-224 jss5686&quot;]/div[@class=&quot;jss5686&quot;]/div[@class=&quot;cell-3-6 jss5698&quot;]/div[@class=&quot;jss5703&quot;]/div[@class=&quot;jss5700&quot;]/h6[@class=&quot;MuiTypography-root-113 jss5701 MuiTypography-subtitle2-125 MuiTypography-colorTextSecondary-139 MuiTypography-alignLeft-128&quot;][count(. | //*[(text() = 'OrigQuote' or . = 'OrigQuote')]) = count(//*[(text() = 'OrigQuote' or . = 'OrigQuote')])]</value>
-      </entry>
-      <entry>
          <key>XPATH</key>
-         <value>//tr[@id='MUIDataTableBodyRow-3']/td[3]/div/div/div/div[2]/h6</value>
+         <value>//tr[@id='MUIDataTableBodyRow-3']/td/div/div/div/div[2]/h6</value>
       </entry>
       <entry>
          <key>CSS</key>
-         <value>div.cell-3-6.jss252 > div.jss257 > div.jss254 > h6.MuiTypography-root-114.jss255.MuiTypography-subtitle2-126.MuiTypography-colorTextSecondary-140.MuiTypography-alignLeft-129</value>
+         <value>div.cell-2-6.jss252 > div.jss257 > div.jss254 > h6.MuiTypography-root-114.jss255.MuiTypography-subtitle2-126.MuiTypography-colorTextSecondary-140.MuiTypography-alignLeft-129</value>
       </entry>
    </selectorCollection>
    <selectorMethod>XPATH</selectorMethod>
@@ -46,48 +42,48 @@
       <matchCondition>equals</matchCondition>
       <name>xpath</name>
       <type>Main</type>
-      <value>id(&quot;MUIDataTableBodyRow-3&quot;)/td[@class=&quot;MuiTableCell-root-214 MuiTableCell-body-216 jss240&quot;]/div[@class=&quot;jss240&quot;]/div[@class=&quot;cell-3-6 jss252&quot;]/div[@class=&quot;jss257&quot;]/div[@class=&quot;jss254&quot;]/h6[@class=&quot;MuiTypography-root-114 jss255 MuiTypography-subtitle2-126 MuiTypography-colorTextSecondary-140 MuiTypography-alignLeft-129&quot;]</value>
+      <value>id(&quot;MUIDataTableBodyRow-2&quot;)/td[@class=&quot;MuiTableCell-root-214 MuiTableCell-body-216 jss240&quot;]/div[@class=&quot;jss240&quot;]/div[@class=&quot;cell-2-6 jss252&quot;]/div[@class=&quot;jss257&quot;]/div[@class=&quot;jss254&quot;]/h6[@class=&quot;MuiTypography-root-114 jss255 MuiTypography-subtitle2-126 MuiTypography-colorTextSecondary-140 MuiTypography-alignLeft-129&quot;]</value>
    </webElementProperties>
    <webElementXpaths>
       <isSelected>true</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath:idRelative</name>
       <type>Main</type>
-      <value>//tr[@id='MUIDataTableBodyRow-3']/td[3]/div/div/div/div[2]/h6</value>
+      <value>//tr[@id='MUIDataTableBodyRow-2']/td/div/div/div/div[2]/h6</value>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath:neighbor</name>
       <type>Main</type>
-      <value>(.//*[normalize-space(text()) and normalize-space(.)='OrigQuote'])[8]/following::h6[1]</value>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='OrigQuote'])[6]/following::h6[1]</value>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath:neighbor</name>
       <type>Main</type>
-      <value>(.//*[normalize-space(text()) and normalize-space(.)='SupportReference'])[9]/following::h6[2]</value>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='OrigQuote'])[5]/following::h6[2]</value>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath:neighbor</name>
       <type>Main</type>
-      <value>(.//*[normalize-space(text()) and normalize-space(.)='OccurrenceNote'])[8]/preceding::h6[1]</value>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='OrigQuote'])[8]/preceding::h6[1]</value>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath:neighbor</name>
       <type>Main</type>
-      <value>(.//*[normalize-space(text()) and normalize-space(.)='Knowledge'])[1]/preceding::h6[2]</value>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='OrigQuote'])[9]/preceding::h6[2]</value>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
       <matchCondition>equals</matchCondition>
       <name>xpath:position</name>
       <type>Main</type>
-      <value>//tr[4]/td[3]/div/div/div/div[2]/h6</value>
+      <value>//tr[3]/td/div/div/div/div[2]/h6</value>
    </webElementXpaths>
 </WebElementEntity>
